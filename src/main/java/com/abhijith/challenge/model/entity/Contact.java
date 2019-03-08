ac622ca33245d50d480fd23b4d6ede8e0d692522
Abhijith <abhijith.komarla@team.telstra.com>
2019-03-08 21:47:45 +1100

    checkstyle issue fixes

--- src/main/java/com/abhijith/challenge/model/entity/Contact.java
+++ src/main/java/com/abhijith/challenge/model/entity/Contact.java
@@ -6,8 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.UUID;
 
 @Data
 @Entity(name = "contact")
